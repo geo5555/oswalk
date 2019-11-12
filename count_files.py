0abde38deeabd284563f4eff0d0e1703232ad439
geo5555 <geo5555@users.noreply.github.com>
2019-11-12 23:50:16 +0200

    change

--- count_files.py
+++ count_files.py
@@ -1,6 +1,8 @@
 import collections
 from pathlib import Path
-c = collections.Counter(p.suffix for p in Path.cwd().iterdir())
+c = collections.Counter(p.suffix for p in Path.cwd().iterdir() if p.is_file())
 print(c)
-c = collections.Counter(p.suffix for p in Path('c:\projects').rglob('*.py'))
+c = collections.Counter(p.suffix for p in Path('d:\projects\rename-files').rglob('*.py') if p.is_file())
+print(c)
+c=collections.Counter([p.suffix for p in Path('e:').rglob("**/*") if p.is_file()])
 print(c)
